359fa1908c7ed6a13a7c13aaf9ccbcceaf4e433c
DiegoRutolo <diegorutolo@protonmail.com>
2019-01-29 22:00:08 +0100

    Implementado juego funcional en dificultad 0

--- src/InterfazConsola.java
+++ src/InterfazConsola.java
@@ -1,8 +1,47 @@
+
+import java.util.Scanner;
+import tresenraya.TresenRaya;
+
 /**
  * @author rutolo
  */
 public class InterfazConsola {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Numero de partidas -> ");
+        int nPart = sc.nextInt();
+        System.out.print("\nDificultad [0,1,2] -> ");
+        int dif = sc.nextInt();
+        System.out.println("\n");
+        int pJug = 0;
+        int pMaq = 0;
+        for (; nPart > 0; nPart--) {
+            TresenRaya tr = new TresenRaya(dif);
+            tr.imprimeTablero();
+            while (tr.hayEspacio()) {
+                tr.jugador(sc.nextInt(), sc.nextInt());
+                tr.imprimeTablero();
+                if (tr.linea(tr.getJug())) {
+                    System.out.println("Punto para el Jugador");
+                    pJug++;
+                    break;
+                }
+                if (tr.linea(tr.getCpu())) {
+                    System.out.println("Punto para la maquina");
+                    pMaq++;
+                    break;
+                }
+            }
+            System.out.println("Fin de la ronda");
+            System.out.println("---\n");
+        }
+        
+        if (pJug == pMaq) {
+            System.out.println("EMPATE");
+        } else if (pJug > pMaq) {
+            System.out.println("GANA EL JUGADOR");
+        } else {
+            System.out.println("GANA LA MAQUINA");
+        }
     }
 }
--- src/tresenraya/TresenRaya.java
+++ src/tresenraya/TresenRaya.java
@@ -1,10 +1,117 @@
 package tresenraya;
 
+import java.util.Random;
+
 /**
  * @author rutolo
  */
 public class TresenRaya {
-    static String hola() {
-        return "Hola mundo";
+    private char[][] tablero = {{'·', '·', '·'}, {'·', '·', '·'}, {'·', '·', '·'}};
+    private int dif;
+    private char cv;       // caracter que indica espacio vacio
+    private char jug = 'X';       // caracter del jugador
+    private char cpu = 'O';       // caracter de la maquina
+    
+    public TresenRaya() {
+        this(0, '·');
+    }
+    
+    public TresenRaya(int dif) {
+        this(dif, '·');
+    }
+    
+    public TresenRaya(int dif, char cv) {
+        this.dif = dif;
+        this.cv = cv;
+    }
+    
+    public char getPos(int x, int y) {
+        return this.tablero[x][y];
+    }
+    
+    public char getJug() {
+        return this.jug;
+    }
+    
+    public char getCpu() {
+        return this.cpu;
+    }
+    
+    public void imprimeTablero() {
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                System.out.print(tablero[x][y]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+    
+    public void jugador(int x, int y) {
+        if (x < 0 || x >= 3 || y < 0 || y >= 3) {
+            return;
+        }
+        
+        if (tablero[x][y] == cv) {
+            tablero[x][y] = jug;
+        }
+        
+        if (this.hayEspacio()) {
+            this.maquina();
+        }
+    }
+
+    public void maquina() {
+        // dificultad 0 -> posicion aleatoria
+        if (this.dif == 0) {
+            Random rnd = new Random();
+            int x, y;
+            do {
+                x = rnd.nextInt(3);
+                y = rnd.nextInt(3);
+            } while (this.tablero[x][y] != cv);
+            this.tablero[x][y] = cpu;
+        }
+    }
+    
+    public boolean hayEspacio() {
+        boolean hay = false;
+        for (char[] i : this.tablero) {
+            for (char j : i) {
+                if (j == cv) {
+                    hay = true;
+                }
+            }
+        }
+        return hay;
+    }
+    
+    public boolean linea(char c) {
+        boolean linea = false;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (this.tablero[x][y] == c) {
+                    if (x == 1) {
+                        if (this.tablero[x-1][y] == c && this.tablero[x+1][y] == c) {
+                            linea = true;
+                        }
+                    }
+                    if (y == 1) {
+                        if (this.tablero[x][y-1] == c && this.tablero[x][y+1] == c) {
+                            linea = true;
+                        }
+                    }
+                    if (x == 1 && y == 1) {
+                        if (this.tablero[x-1][y-1] == c && this.tablero[x+1][y+1] == c) {
+                            linea = true;
+                        }
+                        if (this.tablero[x+1][y-1] == c && this.tablero[x-1][y+1] == c) {
+                            linea = true;
+                        }
+                    }
+                }
+            }
+        }
+        return linea;
     }
 }
